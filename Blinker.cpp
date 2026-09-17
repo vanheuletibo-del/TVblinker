56faa4b1d51210d664d2f22b2c7bc1c4e69dc25f
vanheuletibo <vanheuletibo@gmail.com>
2026-09-17 09:53:43 +0200

    Add files via upload

---
+++ Blinker.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Blinker.h>
+
+                        
+Blinker::Blinker(int pin, unsigned long interval, unsigned long startVertraging = 0) {
+    _pin = pin;
+    _interval = interval;
+    _startVertraging = startVertraging;
+    _volgendeBlink = millis() + _startVertraging;
+
+    pinMode(_pin, OUTPUT);
+}
+
+int Blinker::kweenie(void) {
+    unsigned long nu = millis();
+    int retval = false;
+
+    if (nu >= _volgendeBlink) {
+        _volgendeBlink = nu + _interval;
+        digitalWrite(_pin, !digitalRead(_pin));
+        retval = true;
+    }
+    return retval;
+}
+
---
+++ Blinker.h
@@ -0,0 +1,20 @@
+#ifndef BLINKER_H
+   #define BLINKER_H
+
+    class Blinker{
+        private:
+            int _pin;
+            unsigned long _interval;
+            unsigned long _startVertraging;
+            unsigned long _volgendeBlink;
+
+
+        public:
+            
+            Blinker(int pin, unsigned long interval, unsigned long startVertraging = 0);          //constructor
+            // ~Blinker() ;         //Destructor
+
+            int kweenie();
+    };
+
+#endif
